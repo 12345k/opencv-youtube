f667c871d3de52396b2abf89324a5a3d5596029f
ArvindK <karthickaravindan@ubuntu.ubuntu-domain>
2019-08-04 20:02:49 +0530

    04-08-2019

---
+++ opencv-youtube/background_subtraction_realtime.py
@@ -0,0 +1,39 @@
+import cv2
+import argparse
+
+
+parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
+                                              Opencv2. You can process both videos and images.')
+parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='./video/videoplayback.mp4')
+parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
+args = parser.parse_args()
+
+
+if args.algo == 'MOG2':
+    backSub = cv2.createBackgroundSubtractorMOG2()
+else:
+    backSub = cv2.createBackgroundSubtractorKNN()
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    _,frame = video_capture.read()
+    if frame is None:
+        break
+    
+    fgMask = backSub.apply(frame,-1)
+    
+    
+    cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    cv2.putText(frame, str(video_capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    
+    
+    cv2.imshow('Frame', frame)
+    cv2.imshow('FG Mask', fgMask)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+video_capture.release()
+cv2.destroyAllWindows()
